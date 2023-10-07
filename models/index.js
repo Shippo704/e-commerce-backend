3fe1f0d08ed7132a37914b13e37e30f7eb936b5a
Shippo704 <matt.taylor704@yahoo.ca>
2023-10-07 12:17:06 -0400

    added model relationships in models/index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,36 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+// use foreign key in Product model
+// N:1
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+// use foreign key in Product model
+// 1:N
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+// use relationship table ProductTag
+// use foreign key from Product model
+// N:N
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+// use relationship table ProductTag
+// use foreign key from Tag model
+// N:N
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
